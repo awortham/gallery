6c7170175e946879a607625444c21b5b19c83f8c
awortham <awortham@newlifechurch.tv>
2014-10-29 16:20:28 -0600

    finished with feature

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,7 +3,7 @@ class LineItemsController < ApplicationController
   def create
     cart.add_line_item(params[:id], params[:quantity][:quantity])
     gflash :now, :success => "#{params[:quantity][:quantity]} #{find_item(params[:id]).title}".pluralize + " sucessfully added to your cart!"
-    redirect_to root_path
+    redirect_to photos_path
    end
 
   def update
