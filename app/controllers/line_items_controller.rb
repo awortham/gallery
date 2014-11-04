1412c94b921756c5992706bfc143b1f4b6cd7dcb
Luke Aiken <lukeaiken@gmail.com>
2014-11-04 13:50:17 -0700

    maintain get paramters on adding to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,7 +3,7 @@ class LineItemsController < ApplicationController
   def create
     cart.add_line_item(params[:id], params[:quantity][:quantity])
     gflash :now, :success => "#{params[:quantity][:quantity]} #{find_item(params[:id]).title}".pluralize + " sucessfully added to your cart!"
-    redirect_to photos_path(business.slug)
+    redirect_to request.referer
    end
 
   def update
