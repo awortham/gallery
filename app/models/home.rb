b34d17d2f9512c68ee77c7cc3315af53dd272a91
awortham <awortham@newlifechurch.tv>
2014-11-05 11:57:38 -0700

    added hasattachedfile to home

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,3 +1,4 @@
 class Home < ActiveRecord::Base
   belongs_to :business
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://fillmurray.com/252/158"
 end
